f8d27b2b6c1b78a254a030f2029069e209fdb74c
Pandzul <148607184+Pandzul@users.noreply.github.com>
2025-04-19 22:43:04 +0700

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,24 +1,28 @@
-// lib/db.ts
-import { MongoClient, MongoClientOptions } from "mongodb";
+// lib/mongodb.ts
+import { MongoClient } from 'mongodb';
 
 declare global {
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
+  var _mongoClientPromise: Promise<MongoClient>;
 }
 
-const uri = process.env.MONGODB_URI;
-const options: MongoClientOptions = {};
+const uri = process.env.MONGODB_URI as string;
 
-let client: MongoClient | undefined;
-let clientPromise: Promise<MongoClient> | undefined;
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your MongoDB URI to .env.local");
+if (!uri) {
+  throw new Error('MONGODB_URI tidak ditemukan');
 }
 
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri!, options);
-  global._mongoClientPromise = client.connect();
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
 }
-clientPromise = global._mongoClientPromise as Promise<MongoClient>;
 
 export default clientPromise;
