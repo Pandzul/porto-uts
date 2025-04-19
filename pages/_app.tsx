bf4ecb9677ae671fed950952f4fb012c3e57efec
Pandzul <148607184+Pandzul@users.noreply.github.com>
2025-04-19 21:25:53 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from "next/app";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import "@/styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
